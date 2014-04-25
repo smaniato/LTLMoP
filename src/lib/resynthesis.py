a87aeae472196915434113bd8e6585fe3b12d718
Abhishek Sriraman <as2587@cornell.edu>
2014-04-25 00:17:47 -0400

    Started to process internal propositions (involved with resynthesis)

--- src/lib/resynthesis.py
+++ src/lib/resynthesis.py
@@ -2,7 +2,7 @@ import itertools
 import project
 import re
 import fsa
-import logging
+import parseSpec
 from LTLParser.LTLFormula import LTLFormula, LTLFormulaType
 from createJTLVinput import createLTLfile
 import specCompiler
@@ -17,7 +17,15 @@ class ExecutorResynthesisExtensions(object):
     def __init__(self):
         super(ExecutorResynthesisExtensions, self).__init__()
         logging.info("Initializing Executor Resynthesis")
+        
+        self.next_proj = None
         self.needs_resynthesis = False
+        
+        ####################################################################################
+        # HACK: Wrap the fsa runIteration function to check for internal flags at each step.
+        # (Alternatively, this could be done with handler mapping manipulations or we could
+        # put conditional code inside executor but this is least intrusive for now)
+        ####################################################################################
         
         original_runIteration = self.runStrategyIteration
         check_flags = self._checkForNewInternalFlags
@@ -33,14 +41,102 @@ class ExecutorResynthesisExtensions(object):
             # for flag in self.internalTriggers:
             #     msg += flag + " "
             # logging.info(msg)
+            for p in self.internalTriggers:
+                self._processInternalFlag(p)
             self.internalTriggers = []
         if self.needs_resynthesis:
+            logging.info("Need Resynthesis")
             self.needs_resynthesis = False
+        
+    def _processInternalFlag(self, flag_name):
+        """ Respond appropriately to an "internal flag" proposition having been triggered.
+                    Note that this is only called on rising edges. """
+
+        #################################################
+        ### Check for group modification propositions ###
+        #################################################
+
+        # Use a regex on the proposition name to figure out what we should do
+        m = re.match(r"_(?P<action>add_to|remove_from)_(?P<groupName>\w+)", \
+                     flag_name, re.IGNORECASE)
+            
+        # We currently only handle this one type of flag, so there's nothing to do
+        # if it doesn't match
+        if m is None:
+            return
+        
+        # Create next_proj if we haven't yet
+        # This is what we'll be working on, and then eventually resynthesizing from
+        if self.next_proj is None:
+            d=3
+            #logging.info("Need to duplicate project")
+            #self.next_proj = self._duplicateProject(self.proj)
+
+        # We've been told to add or remove something from a group, but we need to figure
+        # out exactly what that /something/ is (the "referent").
+        response = "new" + flag_name
+        
+        referents = [response]
+        
+        for ref in referents:
+            if m.group('action').lower() == "add_to":
+                logging.info("Added item(s) %s to group %s.", ", ".join(referents), m.group('groupName'))
+                
+                #Rewrite the group definition in the specification text
+                #self._updateSpecGroup(m.group('groupName'), 'add', referents)
+                
+                #Get a list of 
+                # Figure out if there are any corresponding groups which we will also need to update
+                corresponding_groups = parseSpec._findGroupsInCorrespondenceWithGroup(self.proj, m.group('groupName'))
+                logging.info("Need to also update corresponding groups: {}".format(corresponding_groups))
+                
+            
     
-    
+    def _updateSpecGroup(self, group_name, operator, operand):
+            """ Rewrite the text of the specification in `self.next_proj` by modifying proposition
+                group `group_name` with operator `operator` (e.g. "add"/"remove") and
+                operand `operand` (a list of propositions) """
+
+            # Make a regex for finding the group definition in the spec
+            PropositionGroupingRE = re.compile(r"^group\s+%s\s+(is|are)\s+(?P<propositions>.+)\n" % group_name, \
+                                          re.IGNORECASE|re.MULTILINE)
+
+            # Define a function for modifying the list of propositions in-place
+            def gen_replacement_text(group_name, operand, m):
+                """ Given a group name, modification operand, and match object from
+                    PropositionGroupingRE, return a new group definition line with an appropriately
+                    updated list of propositions."""
+
+                # Figure out what propositions are already there
+                propositions = re.split(r"\s*,\s*", m.group('propositions'))
+
+                # Remove the "empty" placeholder if it exists
+                propositions = [p for p in propositions if p != "empty"]
+
+                # Perform the operation on the list of propositions
+                if operator == "add":
+                    propositions.extend(operand)
+                elif operator == "remove":
+                    propositions = [p for p in propositions if p not in operand]
+                else:
+                    logging.error("Unknown group modification operator {!r}".format(operator))
+
+                # Re-add the "empty" placeholder if the result of the operation is now empty
+                if not propositions:
+                    propositions = ["empty"]
+
+                # Create a new group definition line
+                new_group_definition = "group %s is %s\n" % (group_name, ", ".join(propositions))
+
+                return new_group_definition
+
+            self.next_proj.specText = PropositionGroupingRE.sub(lambda m: gen_replacement_text(group_name, operand, m), \
+                                                                self.next_proj.specText)
+                                                                
+                                                                
     def _duplicateProject(self, proj, n=itertools.count(1)):
         """ Creates a copy of a proj, and creates an accompanying spec file with an 
-            auto-incremented counter in the name.  (Not overwriting is mostly for debugging.)"""
+            auto-incremented counter in the name."""
 
         # reload from file instead of deepcopy because hsub stuff can include uncopyable thread locks, etc
         new_proj = project.Project()
